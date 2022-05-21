484a7275546a348a83d056ee1d95c58bec77cfcc
Ilya Khrapko <iamdemos461@gmail.com>
2022-05-21 20:36:00 +0300

    create figures classes, create method for add figures to the board

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     function restart() {
         const newBoard = new Board();
         newBoard.initCells()
+        newBoard.addFigures();
         setBoard(newBoard)
     }
 
--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,39 @@
-export class Figure {
+import {Colors} from "../Colors";
+import logo from '../../assets/black-bishop.png'
+import {Cell} from "../Cell";
 
+export enum FigureNames {
+    FIGURE = 'FIGURE',
+    KING = 'KING',
+    KNIGHT = 'KNIGHT',
+    PAWN = 'PAWN',
+    QUEEN = 'QUEEN',
+    ROOK = 'ROOK',
+    BISHOP = 'BISHOP',
+}
+
+export class Figure {
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureNames;
+    id: number;
+
+
+    constructor(colors: Colors, cell: Cell) {
+        this.color = colors;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureNames.FIGURE;
+        this.id = Math.random();
+    }
+
+    canMove(target: Cell): boolean {
+        return true
+    }
+
+    moveFigure(target: Cell) {
+
+    }
 }
