6f145092623d1f2910f48ed970c104de72852c44
Ilya Khrapko <iamdemos461@gmail.com>
2022-05-22 21:59:17 +0300

    add isEnemy function, add move logic for the pawn and knight

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -21,7 +21,14 @@ export class Cell {
         this.id = Math.random();
     }
 
-    isEmpty() {
+    isEnemy(target: Cell): boolean {
+        if (target.figure) {
+            return this.figure?.color !== target.figure.color
+        }
+        return false;
+    }
+
+    isEmpty(): boolean {
         return this.figure === null;
     }
 
